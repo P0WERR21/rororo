5917d35ca8ad2f0e7f855b1836ed772011eaa676
3bo3c0ewnj9hks <87092008+3bo3c0ewnj9hks@users.noreply.github.com>
2022-04-29 00:57:46 +0300

    Create StandUprightIF.lua

---
+++ StandUprightIF.lua
@@ -0,0 +1,41 @@
+local servicePlr = game:GetService'Players'
+local serviceCGui = game:GetService'CoreGui'
+local serviceTPS = game:GetService'TeleportService'
+local serviceCPI = game:GetService'ContentProvider'
+--
+repeat task.wait() until game:IsLoaded() and game.Players.LocalPlayer.Character and game.Players.LocalPlayer.LoadedIn.Value
+--
+
+local plrLocal = servicePlr.LocalPlayer
+local sameGame = game.PlaceId
+local coolNumber = .3
+
+loadstring(game:HttpGet'https://github.com/sannin9000/scripts/raw/main/Stand%20Upright%20Bypass.lua')() -- creds to sannin
+-- Kick/Error Teleport
+servicePlr.PlayerRemoving:Connect(function(Instance)
+    if Instance == plrLocal then
+        serviceTPS:Teleport(sameGame)
+    end
+end)
+
+serviceCGui.RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(realInstance)
+    if tostring(realInstance) == "ErrorPrompt" and realInstance.MessageArea:FindFirstChild'ErrorFrame' then
+        serviceTPS:Teleport(sameGame)
+    end
+end)
+--
+task.wait()
+firesignal(plrLocal.PlayerGui.MenuGUI.Play.MouseButton1Click)
+repeat task.wait() until not servicePlr.LocalPlayer.PlayerGui.MenuGUI.Enabled and game.Players.LocalPlayer.Character
+repeat
+    for i, v in next, workspace.Items:GetChildren() do
+        local newCFrame = v.Handle.CFrame + Vector3.new(0, -17.3849, 0)
+        local TweenInfo = game.TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(coolNumber, Enum.EasingStyle.Linear), {CFrame = newCFrame}, Enum.EasingDirection.Out, 0, false, 0):Play()
+            task.wait(coolNumber)
+        firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v.Handle, 0)
+    end
+task.wait()
+until #workspace.Items:GetChildren() == 0
+loadstring(game:HttpGet'https://raw.githubusercontent.com/3bo3c0ewnj9hks/rororo/main/ActualServerHop.lua', true)()
+Teleport()
+
