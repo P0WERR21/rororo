d5ba819a173616df4eae9a595d9ec2f8dbb81a39
3bo3c0ewnj9hks <87092008+3bo3c0ewnj9hks@users.noreply.github.com>
2022-07-02 18:03:22 +0300

    Update ybabypass.lua

--- ybabypass.lua
+++ ybabypass.lua
@@ -1,9 +1,9 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/3bo3c0ewnj9hks/rororo/main/ActualServerHop.lua'), true)()
 task.wait()
 local __namecall __namecall = hookmetamethod(game, "__namecall", function(self, ...)
-		local Args = {...}
-		if getnamecallmethod() == "InvokeServer" and tostring(self) == 'Returner' then
-			return "  ___XP DE KEY"
-		end
-		return __namecall(self, ...)
+    local Args = {...}
+	if getnamecallmethod() == "InvokeServer" and tostring(self) == 'Returner' then
+	    return "  ___XP DE KEY"
+	end
+    return __namecall(self, ...)
 end)
