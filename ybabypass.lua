dbbaf46001ffa400df27e51b0d22c4c8c5b366d4
3bo3c0ewnj9hks <87092008+3bo3c0ewnj9hks@users.noreply.github.com>
2022-07-02 18:06:12 +0300

    Update ybabypass.lua

--- ybabypass.lua
+++ ybabypass.lua
@@ -1,4 +1,7 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/3bo3c0ewnj9hks/rororo/main/ActualServerHop.lua'), true)()
+local pService = game:GetService'Players'.LocalPlayer.Character
+repeat task.wait() until game:IsLoaded() and pService and game.ContentProvider.RequestQueueSize == 0
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/3bo3c0ewnj9hks/rororo/main/ActualServerHop.lua', true))()
 task.wait()
 local __namecall __namecall = hookmetamethod(game, "__namecall", function(self, ...)
     local Args = {...}
