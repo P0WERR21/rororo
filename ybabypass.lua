667260865ed8239460b84cac0c0efcc41b11570e
3bo3c0ewnj9hks <87092008+3bo3c0ewnj9hks@users.noreply.github.com>
2022-07-02 18:31:53 +0300

    Update ybabypass.lua

--- ybabypass.lua
+++ ybabypass.lua
@@ -1,5 +1,20 @@
-local pService = game:GetService'Players'.LocalPlayer.Character
-repeat task.wait() until game:IsLoaded() and pService
+local pService = game:GetService'Players'.LocalPlayer
+local charPlayer = pService.Character
+repeat task.wait() until pService.PlayerGui.LoadingScreen or pService.PlayerGui.LoadingScreen1 and charPlayer
+
+local function hitSkip()
+    pcall(function()		
+	local loadingScreen1 = pService.PlayerGui.LoadingScreen1
+        firesignal(loadingScreen1.Frame.LoadingFrame.BarFrame.Skip.TextButton.MouseButton1Click)
+    end)
+end
+
+local function hitPlay()
+    pcall(function()
+	local loadingScreen = pService.PlayerGui.LoadingScreen
+        firesignal(loadingScreen.Play.MouseButton1Click)
+    end)
+end
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/3bo3c0ewnj9hks/rororo/main/ActualServerHop.lua', true))()
 task.wait()
